2174c07da30f30b9970b6485b5bae9362cfd7dbe
Khem Pant <khem0026@edu.easj.dk>
2019-10-10 11:41:30 +0200

    ErrorCommits

--- MandatoryAssignment_Rest/Controllers/BooksController.cs
+++ MandatoryAssignment_Rest/Controllers/BooksController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ItemLibrary;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,18 +12,27 @@ namespace MandatoryAssignment_Rest.Controllers
     [ApiController]
     public class ItemsController : ControllerBase
     {
+        public static List<Book> BookList = new List<Book>
+        {
+            new Book("1234567896543","Popular","FreedomFighter",68,"SocialStudies"),
+            new Book("1239567896542","Khem","CountryLove",108,"SocialStudies"),
+            new Book("1234767896943","Ganga","HealthInstructor",98,"HealthScience"),
+            new Book("1234562965743","Boris","ViceCity",58,"Games"),
+            new Book("1234568896513","Michel","Algebra",68,"Mathematics"),
+            new Book("1234567896842","William","Lyrics",65,"MusicsStudies"),
+        }
         // GET: api/Items
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Book> Get()
         {
-            return new string[] { "value1", "value2" };
+            return BookList;
         }
 
         // GET: api/Items/5
         [HttpGet("{id}", Name = "Get")]
         public string Get(int id)
         {
-            return "value";
+            
         }
 
         // POST: api/Items
