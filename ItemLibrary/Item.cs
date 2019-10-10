17afe18a3dbe5d0ec0c4d98e03371bec4dc33a08
Khem Pant <khem0026@edu.easj.dk>
2019-10-10 11:05:54 +0200

    Commit1

---
+++ ItemLibrary/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ItemLibrary
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Quality { get; set; }
+        public int Quantity { get; set; }
+
+        public Item(int id, string name, string quality, int quantity)
+        {
+            Id = id;
+            Name = name;
+            Quality = quality;
+            Quantity = quantity;
+        }
+        public override string ToString()
+        {
+            return $"{Id}-{Name}-{Quality}-{Quantity}";
+        }
+    }
+}
