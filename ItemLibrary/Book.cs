c45b905295ead153f96c2e837161c8c3f7fda81d
Khem Pant <khem0026@edu.easj.dk>
2019-10-10 11:21:53 +0200

    CommitBookProperties

---
+++ ItemLibrary/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ItemLibrary
+{
+    public class Book
+    {
+        public string IsbnNo { get; set; }
+        public string Author { get; set; }
+        public string Name { get; set; }
+        public int PageNo { get; set; }
+        public string Course { get; set; }
+
+
+        public Book(string isbnNo, string author, string name, int pageNo, string course)
+        {
+            IsbnNo = isbnNo;
+            Author = author;
+            Name = name;
+            PageNo = pageNo;
+            Course = course;
+        }
+        public override string ToString()
+        {
+            return $"{IsbnNo}-{Author}-{Name}-{PageNo}-{Course}";
+        }
+    }
+}
